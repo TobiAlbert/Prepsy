5acddf90e98ae833a2e0ce7f77939d33df1a3eff
tobialbert <daadatobi@gmail.com>
2021-02-10 17:55:54 +0100

    Feat: Create UI for HomePage.

--- app/src/main/java/app/prepsy/ui/splash/SplashActivity.kt
+++ app/src/main/java/app/prepsy/ui/splash/SplashActivity.kt
@@ -2,13 +2,13 @@ package app.prepsy.ui.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import app.prepsy.MainActivity
+import app.prepsy.HomeActivity
 import app.prepsy.utils.startActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity<MainActivity>()
+        startActivity<HomeActivity>()
     }
 }
