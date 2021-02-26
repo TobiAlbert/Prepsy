578ee8ba245a8ed61cf59098250fd880333b2cd8
tobialbert <daadatobi@gmail.com>
2021-02-26 02:09:27 +0100

    Refactor: Remove Kotlin-Android-Extension in favour of Kotlin-Parcelize.

--- app/src/main/java/app/prepsy/ui/models/Question.kt
+++ app/src/main/java/app/prepsy/ui/models/Question.kt
@@ -1,17 +1,16 @@
 package app.prepsy.ui.models
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Option(
     val alphabet: String,
     val text: String
-): Parcelable
+)
 
 @Parcelize
 data class Question(
     val text: String,
     val options: List<Option>,
     val answer: Option
-): Parcelable
+)
