6ef406c3b2ab29d3cb58df85ec7aeb7b961d675a
tobialbert <daadatobi@gmail.com>
2021-04-20 08:22:46 +0100

    Fix: Resolve merge conflicts:

--- app/src/main/java/app/prepsy/utils/Ext.kt
+++ app/src/main/java/app/prepsy/utils/Ext.kt
@@ -51,7 +51,8 @@ fun Int.toAlphabet(): String {
 
     val maxLetterInt = 90
     val baseInt = 64
-    val value = if (this + baseInt > maxLetterInt) 90 else this
+
+    val value = if (this + baseInt > maxLetterInt) maxLetterInt else this
 
     return (baseInt + value).toChar().toString()
 }
