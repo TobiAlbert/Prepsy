523554f754b4c718a207b154ae40e769c0415f53
tobialbert <daadatobi@gmail.com>
2021-04-22 23:24:29 +0100

    Refactor: Use extension function to retrieve fetching of drawable.

--- app/src/main/java/app/prepsy/ui/questions/adapters/QuestionNavAdapter.kt
+++ app/src/main/java/app/prepsy/ui/questions/adapters/QuestionNavAdapter.kt
@@ -7,8 +7,8 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.annotation.LayoutRes
-import androidx.core.content.ContextCompat
 import app.prepsy.R
+import app.prepsy.utils.getDrawableCompat
 
 
 class QuestionNavAdapter(
@@ -27,14 +27,8 @@ class QuestionNavAdapter(
         view.findViewById<TextView>(R.id.questionIndex).apply {
             text = questions[position].toString()
             background = when ((0..1).random()) {
-                0 -> ContextCompat.getDrawable(
-                        context,
-                        R.drawable.drawable_question_answered_background
-                    )
-                else -> ContextCompat.getDrawable(
-                            context,
-                            R.drawable.drawable_question_unanswered_background
-                        )
+                0 -> context.getDrawableCompat(R.drawable.drawable_question_answered_background)
+                else -> context.getDrawableCompat(R.drawable.drawable_question_unanswered_background)
             }
             setOnClickListener { onQuestionSelected(position) }
         }
